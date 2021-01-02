dec125683dcde6a1ac91ae26aadcde8dbd17f404
Filippo Airaldi <filippoairaldi@gmail.com>
2021-01-02 10:46:45 +0100

    Added new icon sizes

--- scripts/background.js
+++ scripts/background.js
@@ -4,13 +4,17 @@ var imgs_on = {
 	16: "icons/icon-16-on.png",
 	32: "icons/icon-32-on.png",
 	48: "icons/icon-48-on.png",
-	96: "icons/icon-96-on.png"
+	64: "icons/icon-64-on.png",
+	96: "icons/icon-96-on.png",
+	128: "icons/icon-128-on.png"
 };
 var imgs_off = {
 	16: "icons/icon-16-off.png",
 	32: "icons/icon-32-off.png",
 	48: "icons/icon-48-off.png",
-	96: "icons/icon-96-off.png"
+	64: "icons/icon-64-off.png",
+	96: "icons/icon-96-off.png",
+	128: "icons/icon-128-off.png"
 };
 
 function onError(error) {
