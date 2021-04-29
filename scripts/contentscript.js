1b94aeadf02f23e601d7cd3bdca4864b4b018d40
Filippo Airaldi <filippoairaldi@gmail.com>
2021-04-29 22:57:46 +0200

    now closes popup tab

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,12 +2,27 @@
 
 var intervalId = null;
 
-function performClick() {
-	document
-		.querySelector('#top-toolbar > colab-connect-button')
-		.shadowRoot
-		.querySelector('#connect')
-		.click();	
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function performClick() {    
+    var connectBtn = document
+ 		?.querySelector('#top-toolbar > colab-connect-button')
+ 		?.shadowRoot
+ 		?.querySelector('#connect')
+    if (connectBtn === null || connectBtn === undefined) {
+        return;
+    }
+    connectBtn.click();
+    
+    await sleep(500);
+    
+    var closeBtn = document?.querySelector('paper-icon-button[title=Close]');
+    if (connectBtn === null || connectBtn === undefined) {
+        return;
+    }
+    closeBtn.click(); 
 }
 
 browser.runtime.onMessage.addListener(() => {
@@ -18,5 +33,5 @@ browser.runtime.onMessage.addListener(() => {
 		clearInterval(intervalId);
 		intervalId = null;
 	}
-	return Promise.resolve(intervalId !== null);
+ 	return Promise.resolve(intervalId !== null);
 });
